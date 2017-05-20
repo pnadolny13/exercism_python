ac5aa6d464f02d6d1aa87b49db216ddfd9c7ee51
pnadolny13 <patnadolny@gmail.com>
2017-05-20 14:15:52 -0400

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,1 +1,18 @@
-def is_pangram():
+def is_pangram(x):
+    x = str.lower(x);
+    sentence = set();
+    count = 0;
+    for i in x:
+        sentence.update(x[count])
+        count = count +1;
+    import string
+    alphabet = set(string.ascii_lowercase)
+    #print (alphabet);
+    #print (sentence);
+    for i in alphabet:
+          if i not in sentence:
+              return False;
+              break;
+    return True;
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy 
