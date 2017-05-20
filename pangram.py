d62c580d396479c5162806907cd16ebc14bcb11f
pnadolny13 <patnadolny@gmail.com>
2017-05-20 14:12:17 -0400

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,2 +1,16 @@
-def is_pangram():
-#working on this
+def is_pangram(x):
+    x = str.lower(x);
+    sentence = set();
+    count = 0;
+    for i in x:
+        sentence.update(x[count])
+        count = count +1;
+    import string
+    alphabet = set(string.ascii_lowercase)
+    #print (alphabet);
+    #print (sentence);
+    for i in alphabet:
+          if i not in sentence:
+              return False;
+              break;
+    return True;
